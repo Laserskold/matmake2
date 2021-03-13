1bafa7f4fdba2d6bb9e770b40badafe635b7bfae
Mattias Lasersköld <mattias.l.sk@gmail.com>
2021-03-13 11:27:44 +0100

    Make linux script guess compiler #40

--- build-linux.sh
+++ build-linux.sh
@@ -2,6 +2,47 @@ mkdir -p build
 
 CXX=$1
 
+if [ -z "$CXX" ]
+then
+	echo No compiler specified: Guessing...
+
+	# Select the latest installed version of clang++ or g++
+	for ((i = 20; i >= 6; --i))
+	do
+		CLANG="clang++-$i"
+		if ! ! command -v ${CLANG} &> /dev/null
+		then
+			echo found $CLANG
+			CXX=$CLANG
+			break;
+		fi
+
+		GCC="g++-$1"
+		if ! ! command -v ${GCC} &> /dev/null
+		then
+			echo found $GCC
+			CXX=$GCC
+			break;
+		fi
+	done;
+
+
+	# Test without numbers
+	if [ -z "$CXX" ]
+	then
+		if ! ! command -v clang++ &> /dev/null
+		then
+			echo found clang++
+			CXX=clang++
+		elif ! ! command -v g++ &> /dev/null
+		then
+			echo found g++
+			CXX=g++
+		fi
+	fi
+fi
+
+
 echo building with ${CXX}...
 
 ${CXX} -o build/matmake2 matmake.cpp \
